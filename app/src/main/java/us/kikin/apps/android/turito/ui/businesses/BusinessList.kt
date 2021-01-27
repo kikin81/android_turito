22de416b5d5e8a9e13db32a3bdc00268c22176d1
Francisco Velazquez <velazquez@avast.com>
2021-01-27 12:45:15 -0800

    Added navigation component. Added view binding

---
+++ app/src/main/java/us/kikin/apps/android/turito/ui/businesses/BusinessList.kt
@@ -0,0 +1,27 @@
+package us.kikin.apps.android.turito.ui.businesses
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import us.kikin.apps.android.turito.R
+
+class BusinessList : Fragment() {
+
+    private lateinit var viewModel: BusinessListViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.business_list_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(BusinessListViewModel::class.java)
+    }
+}
---
+++ app/src/main/java/us/kikin/apps/android/turito/ui/businesses/BusinessListViewModel.kt
@@ -0,0 +1,5 @@
+package us.kikin.apps.android.turito.ui.businesses
+
+import androidx.lifecycle.ViewModel
+
+class BusinessListViewModel : ViewModel()
